4db47d7fe59e8ebd5bb4c4e93ae22e1e74746925
dandybytes <dandybytes@gmx.com>
2019-11-24 16:46:51 +0200

    create HomePage layout & design

--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Title.scss";
 
-const Title = ({text, center, uppercase}) => {
+const Title = ({text, center, uppercase, className}) => {
     const titleStyle = {};
     if (center) titleStyle.textAlign = "center";
     if (uppercase) titleStyle.textTransform = "uppercase";
 
     return (
-        <h1 className="title" style={titleStyle}>
+        <h1 className={className} style={titleStyle}>
             {text}
         </h1>
     );
@@ -21,7 +21,7 @@ Title.propTypes = {
 };
 
 Title.defaultProps = {
-    // text: ""
+    className: "title"
 };
 
 export default Title;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import Title from "./../common/Title";
-// import "./HomePage.scss";
+import Title from "../common/Title";
+import LinkButton from "../common/LinkButton";
+import ForwardIcon from "./../icons/ForwardIcon";
+import "./HomePage.scss";
+import RightArrowIcon from "../icons/RightArrowIcon";
 
 const HomePage = () => {
     return (
         <div className="home-page">
-            <Title text="Home page." center></Title>
+            <main>
+                <div className="home-page-headline">
+                    <Title text="Image Converter" center className="home-page-title"></Title>
+                    <Title text="RGB to ASCII" center className="home-page-subtitle"></Title>
+                </div>
+                <div className="home-page-demo-rgb">
+                    <RightArrowIcon fill={"var(--mainColor)"} />
+                </div>
+                <div className="home-page-demo-ascii"></div>
+                <div className="home-page-link-image">
+                    <LinkButton destination="/image">
+                        <span>Image Converter</span>
+                        <ForwardIcon fill={"var(--darkColor)"} />
+                    </LinkButton>
+                </div>
+                <div className="home-page-link-camera">
+                    <LinkButton destination="/camera">
+                        <span>Camera Converter</span>
+                        <ForwardIcon fill={"var(--darkColor)"} />
+                    </LinkButton>
+                </div>
+            </main>
         </div>
     );
 };
