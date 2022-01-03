59653c52ecc43bbb8a81bd1a3aa35bd609aebf8c
dandybytes <dandybytes@gmx.com>
2022-01-03 17:41:04 +0100

    Images: public dir --> src dir

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+
 import Title from "../common/Title";
 import LinkButton from "../common/LinkButton";
-import ForwardIcon from "./../icons/ForwardIcon";
+
 import "./HomePage.scss";
+import sampleRGBimage from "../../assets/img/example-rgb.jpg";
+import sampleASCIIimage from "../../assets/img/example-ascii.png";
+import ForwardIcon from "./../icons/ForwardIcon";
 import RightArrowIcon from "../icons/RightArrowIcon";
 
 const HomePage = () => {
@@ -13,10 +17,17 @@ const HomePage = () => {
                     <Title text="Image Converter" center className="home-page-title"></Title>
                     <Title text="RGB to ASCII" center className="home-page-subtitle"></Title>
                 </div>
-                <div className="home-page-demo-rgb">
+                <div
+                    className="home-page-demo-rgb"
+                    style={{backgroundImage: `url(${sampleRGBimage})`}}
+                >
                     <RightArrowIcon fill={"var(--mainColor)"} />
                 </div>
-                <div className="home-page-demo-ascii"></div>
+                <div
+                    className="home-page-demo-ascii"
+                    style={{backgroundImage: `url(${sampleASCIIimage})`}}
+                ></div>
+
                 <div className="home-page-link-image">
                     <LinkButton destination="/image">
                         <span>Image Converter</span>
